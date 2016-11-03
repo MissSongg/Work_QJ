9846edd07f7c57aa60ab7aa8f7fdb183a96e8d33
一帆风顺 <zf253944001@163.com>
2016-11-03 09:57:53 +0800

    修改bug

---
+++ QJY.API/API/DBGL/DBGLManage.cs
@@ -0,0 +1,24 @@
+﻿using QJY.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using FastReflectionLib;
+using System.Data;
+using QJY.Data;
+using Newtonsoft.Json;
+using Senparc.Weixin.QY.Entities;
+
+namespace QJY.API.API.DBGL
+{
+    public class DBGLManage : IWsService
+    {
+        public void ProcessRequest(HttpContext context, ref Msg_Result msg, string P1, string P2, JH_Auth_UserB.UserInfo UserInfo)
+        {
+            MethodInfo methodInfo = typeof(DCGLManage).GetMethod(msg.Action.ToUpper());
+            DBGLManage model = new DBGLManage();
+            methodInfo.FastInvoke(model, new object[] { context, msg, P1, P2, UserInfo });
+        }
+    }
+}
